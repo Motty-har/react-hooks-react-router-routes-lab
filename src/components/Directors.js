ed7628755e944625f5b254f16e4b4c66d30aca91
Motty-har <harligmotty@gmail.com>
2023-07-12 20:13:15 -0400

    Completed

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,25 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return(
+    <div>
+      <h1>Directors Page</h1>
+      <div>
+        {directors.map((director) => {
+          return(
+            <div>
+              <h3>{director.name}</h3>
+              <ul>
+                {director.movies.map((movie) => {
+                  return <li>{movie}</li>
+                })}
+              </ul>
+            </div>
+          )
+
+        })}
+      </div>
+   </div>)
 }
 
 export default Directors;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,23 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <NavLink exact to="/">
+        Home
+      </NavLink><br></br>
+      <NavLink exact to="/movies">
+        Movies
+      </NavLink><br></br>
+      <NavLink exact to="/directors">
+        Directors
+      </NavLink><br></br>
+      <NavLink exact to="/actors">
+        Actors
+      </NavLink>
+    </div>
+  )
 }
 
 export default NavBar;
